ccf7facca8e9458a91a9bacefdabac97e951998a
vishal sarsani <vsarsani@umass.edu>
2022-06-08 11:08:10 -0400

    first commit

---
+++ R/lasso.R
@@ -0,0 +1,24 @@
+#' Title
+#'
+#' @param fit 
+#' @param varsVec 
+#' @param covarsVec 
+#' @param covInformation 
+#' @param catCovariates 
+#' @param restart 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+lassoVar <- function(fit,
+           varsVec,
+           covarsVec,
+           covInformation = NULL,
+           catCovariates = NULL,
+           restart = FALSE){
+    
+    
+    
+    
+  }
