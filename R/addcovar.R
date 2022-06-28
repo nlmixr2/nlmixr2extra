2391b21016617252dd916d31c37440101c31e52e
vishal sarsani <vsarsani@umass.edu>
2022-06-28 16:06:30 -0400

    add covariate

---
+++ R/addcovar.R
@@ -0,0 +1,93 @@
+#' lasso method for predictive covariate model building
+#' lasso is suggested over SCM to remedy selection bias ,poor 
+#' predictive performance and speed 
+#' 
+#' Initializing covariates before estimation
+#' 
+#' 
+#' @param ui compiled rxode2 nlmir2 model or fit  
+#' @param varName  the variable name to which the given covariate is to be added
+#' @param covariate the covariate that needs string to be constructed 
+addCovariate <- function(ui,varName,covariate) {
+  
+  if (inherits(ui, "nlmixr2FitCore")) {
+    ui <- ui$ui
+  }
+  ui <- rxode2::assertRxUi(ui)
+  
+  
+  checkmate::assertCharacter(varName,len = 1,any.missing = FALSE )
+  checkmate::assertCharacter(covariate,len = 1,any.missing = FALSE )
+  
+  if (inherits(try(str2lang(varName)),"try-error")){
+    stop("`varName` must be a valid R expression",call. = FALSE)
+  }
+  if (inherits(try(str2lang(covariate)),"try-error")){
+    stop("`varName` must be a valid R expression",call. = FALSE)
+  }
+  .pop <- .getThetaName(ui,varName=varName)
+  
+  .cov <- paste0("cov_", covariate, "_", varName)
+  .covdf <- rbind(ui$muRefCovariateDataFrame,
+                  data.frame(theta=.pop,covariate=covariate,covariateParameter=.cov))
+  .split <- ui$getSplitMuModel
+  .pars <- c(names(.split$pureMuRef),names(.split$taintMuRef))
+  .model <- nlmixr2est::.saemDropMuRefFromModel(ui)
+  lapply(.model,.expandRefMu,murefDf=ui$muRefDataFrame,covDf=.covdf,pars=.pars)
+}
+
+
+
+
+.expandRefMu <- function(x,murefDf,covDf,pars) {
+  if (is.name(x)) {
+    currparam <- as.character(x)
+    if (currparam %in% pars){
+      return (str2lang(.expandPopExpr(currparam,murefDf,covDf)))
+    } 
+  }else if(is.call(x)) {
+    return(as.call(c(list(x[[1]]), lapply(x[-1], .expandRefMu, murefDf=murefDf, covDf=covDf, pars=pars))))
+  }
+  x
+}
+  
+
+
+.expandPopExpr <- function(popParam,murefDf,covDf) {
+  
+  .par1 <- murefDf[murefDf$theta==popParam,]
+  .res <- paste0(.par1$theta,"+",.par1$eta)
+  .w <- which(covDf$theta==popParam)
+  if(length(.w)>0){
+    .res <- paste0(c(.res,paste0(covDf[.w,"covariateParameter"],"*",covDf[.w,"covariate"])),
+                   collapse="+") 
+    
+  }
+  .res
+}
+
+
+
+
+#' get the population parameter from variable name
+#'
+#' @param ui compiled rxode2 nlmir2 model or fi
+#' @param varName the variable name to which the given covariate is to be added
+#'
+#' @return population parameter variable
+#' @noRd
+.getThetaName <- function(ui,varName){
+  .split <-  ui$getSplitMuModel
+  if (varName %in% names(.split$pureMuRef)){
+    return(varName)
+  }
+  .w <- which(.split$pureMuRef==varName)
+  if(length(.w)==1){
+    return(names(.split$pureMuRef)[.w])  
+  }
+  if (varName %in% names(.split$taintMuRef)){
+    return(varName)
+  }
+  stop("'",varName,"'","has not been found in the model ui",call. = FALSE)
+}
+
