4e2fb23e629bc2d8a5a3eccd9deb7fab0b8e46fe
matt <matthew.fidler@gmail.com>
2022-09-22 21:22:36 -0500

    Add theo fit ode

---
+++ R/theoFitOde.R
@@ -0,0 +1,11 @@
+#' Example single dose Theophylline ODE model 
+#'
+#' This is a nlmixr2 model that is pre-run so that it can be used in
+#' package testing and development.  It is regenerated whenever
+#' binaries of `nlmixr2extra` are created.  If there is a binary
+#' incompatability between the fit objects, a simple rerun of the
+#' installation will fix this nlmixr2 fit object.
+#'
+#'
+#' @format A nlmixr2 fit object, which is a modified data frame with XXX rows and 5 columns
+"theoFitOde"
