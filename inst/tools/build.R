ed06f3f850db34fa5de683d97790f6ec7f86768c
Matthew Fidler <matthew.fidler@gmail.com>
2024-11-21 12:32:51 -0600

    White list PopOS!

--- inst/tools/build.R
+++ inst/tools/build.R
@@ -3,13 +3,23 @@ if (!dir.exists("data")) {
 }
 
 .in <- suppressWarnings(readLines("src/Makevars.in"))
-if (.Platform$OS.type == "windows" && !file.exists("src/Makevars.win") ||
-    (R.version$os == "linux-musl")) {
-  file.out <- file("src/Makevars.win", "wb")
-  writeLines(.in, file.out)
-  close(file.out)
+if (.Platform$OS.type == "windows") {
+  .makevars <- file("src/Makevars.win", "wb")
+  .i <- "I"
 } else {
-  file.out <- file("src/Makevars", "wb")
-  writeLines(.in, file.out)
-  close(file.out)
+  .makevars <- file("src/Makevars", "wb")
+  if (file.exists("/etc/os-release")) {
+    .os <- readLines("/etc/os-release")
+    if (any(grepl("Pop!_OS", .os, fixed=TRUE))) {
+      .i <- "isystem"
+    } else {
+      .i <- "I"
+    }
+  } else {
+    .i <- "I"
+  }
 }
+
+writeLines(gsub("@ISYSTEM@", .i, .in),
+           .makevars)
+close(.makevars)
